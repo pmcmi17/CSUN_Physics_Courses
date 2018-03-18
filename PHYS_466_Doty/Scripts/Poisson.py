0300f5ca3e77d641213c7cf0e2172e9c5323a5bc
Patrick Joseph Mcmillin <patrick.joseph.mcmillin@cern.ch>
2018-03-18 10:09:40 -0700

    Added various scripts and project requirements and data.

--- PHYS_466_Doty/Scripts/Poisson.py
+++ PHYS_466_Doty/Scripts/Poisson.py
@@ -1,42 +1,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
+import scipy as sp
 
-# List index is the number of counts.
-# List value is the number of instances of the
-# count number being observed.
+Raw_Data = [23, 48, 24, 19, 4, 2]
 
-Binned_Data = [23, 48, 24, 19, 4, 2]
-# Initial guesses for the Poisson fitting
-x0 = []
+Average = 0
+for i in range(len(Raw_Data)):
+    Average += (i * Raw_Data[i])
+Average = Average / float(sum(Raw_Data))
 
-# chi-squared function to minimize
-def chi_squared(x0):
-    # Defines the poisson function
-    poisson = lambda x: x
-    T = []
-    chi_sq = []
-    for i in range(len(Binned_Data)):
-        T_i = poisson(i)
-        T.append(T_i)
-        chi_sq.append((T_i-Binned_Data[i])**2/T_i)
-    Chi_Squared = sum(chi_sq)
-    return Chi_Squared
-# Calls the minimizer
-#Min = opt.minimize(chi_squared,x0)
-# gets the minimized values
-#x_opt = Min.x
+Std = np.sqrt(Average)
 
-# Plots the fitted function and the experimental data. 
-plt.figure(1)
-x = np.linspace(-0.5,5.5,1000)
-# Defines the poisson function to plot.
-#poisson = lambda x: x
-plt.scatter(range(len(Binned_Data)),Binned_Data,
-         color= 'red', label='Collected Data')
-#plt.plot(x,poisson(x), color='blue',label='Fitted Function')
+Norm_Fact = sum(Raw_Data)
+Average_opt = 1.46738148
+
+poisson = lambda r: Norm_Fact * np.exp(-Average_opt) * (Average_opt ** r) / float(sp.special.factorial(r))
+
+Raw_Theo = []
+Raw_Partial_Chi = []
+for i in range(len(Raw_Data)):
+    Raw_Theo.append(poisson(i))
+    Raw_Partial_Chi.append(((Raw_Theo[i] - Raw_Data[i]) ** 2) / Raw_Theo[i])
+Raw_Chi_Squared = sum(Raw_Partial_Chi)
+
+print(Raw_Data)
+print(Raw_Theo)
+print(Raw_Partial_Chi)
+print(Raw_Chi_Squared)
+
+Raw_x = range(len(Raw_Data))
+plt.scatter(Raw_x, Raw_Data, color = 'red', label = 'Collected Data')
+plt.scatter(Raw_x, Raw_Theo, color = 'blue', label = 'Fitted Poisson Function', marker = 's')
 plt.xlim(-0.5, 5.5)
-plt.title('Poisson Data')
+plt.title('Distribution of Counts in LO 1100')
 plt.xlabel('Counts')
+plt.text(4.2, 55, 'McMillin, Patrick')
+plt.text(3, 40, r'$\mu = $' + str(round(Average, 2)))
+plt.text(3, 37, r'$\sigma = $' + str(round(Std, 2)))
+plt.text(3, 34, r'$\chi^2 = $' + str(round(Raw_Chi_Squared, 2)))
+plt.text(3, 31, r'$DoF = 6 - 2 = 4$')
+plt.text(3, 28, r'$\chi^2_r = $' + str(round(Raw_Chi_Squared / 4, 2)))
+plt.text(3, 25, 'Accept the fit (23% - 31%)')
 plt.legend()
+plt.savefig('Plots/Raw_Poisson.pdf')
 plt.show()
+
+New_Data_Bin = Raw_Data[4] + Raw_Data[5]
+Significant_Data = Raw_Data
+del(Significant_Data[5])
+del(Significant_Data[4])
+Significant_Data.append(New_Data_Bin)
+
+New_Theo_Val = Raw_Theo[4] + Raw_Theo[5]
+Significant_Theo = Raw_Theo
+del(Significant_Theo[5])
+del(Significant_Theo[4])
+Significant_Theo.append(New_Theo_Val)
+
+New_Partial_Chi_Val = Raw_Partial_Chi[4] + Raw_Partial_Chi[5]
+Significant_Partial_Chi = Raw_Partial_Chi
+del(Raw_Partial_Chi[5])
+del(Raw_Partial_Chi[4])
+Significant_Partial_Chi.append(New_Partial_Chi_Val)
+
+Significant_Chi_Squared = sum(Significant_Partial_Chi)
+
+print(Significant_Data)
+print(Significant_Theo)
+print(Significant_Partial_Chi)
+print(Significant_Chi_Squared)
+
+plt.clf()
+
+Significant_x = range(len(Significant_Data))
+plt.scatter(Significant_x, Significant_Data, color = 'red', label = 'Collected Data')
+plt.scatter(Significant_x, Significant_Theo, color = 'blue', label = 'Fitted Poisson Function', marker = 's')
+plt.xlim(-0.5, 5.5)
+plt.title('Distribution of Counts in LO 1100')
+plt.xlabel('Counts')
+plt.text(4.2, 55, 'McMillin, Patrick')
+plt.text(3, 40, r'$\mu = $' + str(round(Average, 2)))
+plt.text(3, 37, r'$\sigma = $' + str(round(Std, 2)))
+plt.text(3, 34, r'$\chi^2 = $' + str(round(Raw_Chi_Squared, 2)))
+plt.text(3, 31, r'$DoF = 5 - 2 = 3$')
+plt.text(3, 28, r'$\chi^2_r = $' + str(round(Raw_Chi_Squared / 3, 2)))
+plt.text(3, 25, 'Accept the fit (14% - 19%)')
+plt.legend()
+plt.savefig('Plots/Significant_Poisson.pdf')
+plt.show()
+    
